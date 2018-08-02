92da68e89d0593ee65eb7f74ce6533e8b762dc9c
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2018-08-02 17:21:33 +1000

    x

--- examples/double_robust_estimators/config.py
+++ examples/double_robust_estimators/config.py
@@ -149,14 +149,14 @@ def d(cmpl_rates, pred_rates, train_obs, propensities, omega, risk):
   true_errors = risk(pred_rates - cmpl_rates)
 
   #### true error
-  pred_errors = np.copy(true_errors)
+  pred_errors = 0.6 * np.copy(true_errors)
   #### mean error
   # pred_errors = np.mean(true_errors) * np.ones_like(true_errors)
   #### mean rate
   # pred_errors = risk(pred_rates - np.mean(cmpl_rates))
   #### pred omega
   # omega = true_errors.sum() / risk(pred_rates - np.mean(cmpl_rates)).sum()
-  pred_errors = omega * pred_errors
+  # pred_errors = omega * pred_errors
   pred_errors = np.multiply(propensities-train_obs, pred_errors)
   pred_errors = np.divide(pred_errors, propensities)
 
